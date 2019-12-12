cc6004531d3c5cab51cb8d39cb6beaec1dc6b926
Karl Jastin P. Dilao <58801401+karljastin@users.noreply.github.com>
2019-12-12 14:34:35 +0800

    Add files via upload

---
+++ DILAO LABEXERCISE A.java
@@ -0,0 +1,15 @@
+public class LabExer1A
+{
+	public static void main(String[]args)
+	{
+		int FaveNumber = 27 ;
+		String FaveCartoonCharacter = "Ghost Fighter!";
+		char Mi = 'P';
+		char[]NickName = {'K','a','r','l'};
+		
+		System.out.println(FaveNumber+ " is my favorite number. ");
+		System.out.println("I love " + FaveCartoonCharacter );
+		System.out.println(Mi+" is my middle innitial. ");
+		System.out.println("You can call me " + NickName[0] + NickName[1] + NickName[2] + NickName[3]);
+	}
+}
